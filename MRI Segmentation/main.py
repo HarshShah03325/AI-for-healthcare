07b566d82f8e3a27bbac4b6d292f0acad29f1671
HarshShah03325 <shahh2773@gmail.com>
2021-10-29 23:38:12 +0530

    updated main

--- MRI Segmentation/main.py
+++ MRI Segmentation/main.py
@@ -1,5 +1,6 @@
-from preprocess import load_data, result, visualize_result
+from preprocess import load_case, load_data, result, visualize_result
 import argparse
+from metrics import compute_metrics, get_sens_spec_df
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
@@ -12,6 +13,10 @@ image_file = args["image"]
 label_file = args["label"]
 
 X, y = load_data(image_file, label_file)
+
 pred = result(X)
+
+metrics = get_sens_spec_df(pred[0],y)
+print(metrics)
 
 visualize_result(X,pred,y)
